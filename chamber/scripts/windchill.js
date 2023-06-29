3c55c3d85c1adf077b488db7e808f013f1613065
I-RICHWEB <ize20002@byui.edu>
2023-06-29 10:30:55 +0100

    Spotlight update

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -37,7 +37,6 @@ function displayInfo(info) {
 }
 
 function windChill(temp, speed) {
-
   const fahrenheit = (temp * 9) / 5 + 32;
 
   const mph = speed / 1.609;
@@ -57,10 +56,105 @@ function windChill(temp, speed) {
   return chill;
 }
 
-const weatherLink = document.querySelector('.map');
+const weatherLink = document.querySelector(".map");
 const siteLink = "https://openweathermap.org/";
-const link = document.createElement('a');
-link.setAttribute('href', siteLink);
+const link = document.createElement("a");
+link.setAttribute("href", siteLink);
 link.setAttribute("target", "blank");
 link.textContent = "openweathermap.org";
 weatherLink.appendChild(link);
+
+//I am going to code the dynamic spotlight javascript here.
+const spotlight = document.querySelector(".spotlight");
+
+function outputSpotlight(datas) {
+  const spotlight = document.querySelector(".spotlight");
+
+  //Clear the spotlight
+  spotlight.innerHTML = "";
+
+  //Empty array for the random.
+  const selection = [];
+
+  for (i = 0; i < 3; i++) {
+    let theLength = datas.length;
+    let theRandom = Math.floor(Math.random() * theLength);
+    if (selection.includes(theRandom)) {
+      theRandom = Math.floor(Math.random() * theLength);
+      break;
+    } else {
+      selection.push(theRandom);
+      let theData = datas[theRandom];
+
+      if (
+        theData.membershipLevel === "gold" ||
+        theData.membershipLevel === "silver"
+      ) {
+        //console.log('it got here!')
+        outWork(theData);
+      } else {
+        theRandom = Math.floor(Math.random() * theLength);
+        theData = datas[theRandom];
+        outWork(theData);
+      }
+    }
+  }
+  selection.length = 0;
+}
+
+function outWork(theData) {
+  const divContainer = document.createElement("div");
+  //Create new elements.
+  const name = document.createElement("h2");
+  name.textContent = theData.name;
+  //Creating the image and set all it values.
+  const image = document.createElement("img");
+  image.setAttribute("src", theData.image);
+  image.setAttribute("alt", `${theData.name} logo.`);
+  image.setAttribute("loading", "lazy");
+  image.setAttribute("width", "100");
+  image.setAttribute("height", "100");
+  //Creating the rest of the things.
+  const address = document.createElement("p");
+  address.textContent = theData.address;
+
+  const breaker = document.createElement("hr");
+
+  // Creating the phone contact.
+  const phone = document.createElement("p");
+  phone.textContent = theData.PhoneNumber;
+  //Creating the link and setting it attribute.
+  const link = document.createElement("a");
+  link.setAttribute("href", theData.url);
+  link.setAttribute("target", "blank");
+  link.textContent = "website";
+
+  // Appending the elements to the div container.
+  divContainer.appendChild(name);
+  divContainer.appendChild(image);
+  divContainer.appendChild(address);
+  divContainer.appendChild(breaker);
+  //creating the email with condition.
+  if (theData.emailAddress !== null) {
+    const email = document.createElement("p");
+    email.textContent = theData.emailAddress;
+    email.classList.add("email");
+    divContainer.appendChild(email);
+  }
+  // The end of email.
+  divContainer.appendChild(phone);
+  divContainer.appendChild(link);
+
+  // Appending the container to the section.
+  spotlight.appendChild(divContainer);
+}
+
+const fileJson = "./json_file/data.json";
+getdata(fileJson);
+
+async function getdata(file) {
+  const info = await fetch(file);
+  const data = await info.json();
+  //console.log(data);
+  outputSpotlight(data);
+}
